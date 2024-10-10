b4823cee38c857bd81fd010fd8354a06d279ae30
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-10 15:14:35 +0200

    initial_commit

---
+++ assets/main.js
@@ -0,0 +1,18 @@
+console.log('its works');
+
+
+/* 
+Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente deve inserire i numeri che ha visto precedentemente, nell'ordine che preferisce.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+/* 
+Consigli del giorno:
+Pensate prima in italiano.
+Dividete in piccoli problemi la consegna.
+Individuate gli elementi di cui avete bisogno per realizzare il programma.
+Immaginate la logica come fosse uno snack: "Dati 2 array di numeri, indica quali e quanti numeri ci sono in comune tra i due array"
+*/
